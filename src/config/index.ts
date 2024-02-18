5a0c59381ed155c16f79e10c0ae7ca1da6af7bd5
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-02-18 12:08:04 +0600

    jwt impleament and after checking user info validation send access token to the client functionality added

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,6 @@ export default {
   NODE_ENV: process.env.NODE_ENV,
   database_url: process.env.DATABASE_URL,
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import httpStatus from 'http-status';
+import config from '../../config';
 import AppError from '../../errors/AppError';
 import User from '../user/user.model';
 import { TLogin } from './auth.interface';
+import { createToken } from './auth.utils';
 
 const loginUser = async (payload: TLogin) => {
   console.log(payload);
@@ -9,16 +11,38 @@ const loginUser = async (payload: TLogin) => {
   if (!isUserExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'User not found!');
   }
+  const user = isUserExists;
+
+  if (user.status === 'block') {
+    throw new AppError(httpStatus.FORBIDDEN, 'Your account is block!');
+  }
+
+  if (user.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Your account is deleted');
+  }
 
   const isPasswordMatched = await User.isPasswordMatched(
     payload.password,
-    isUserExists.password,
+    user.password,
   );
   if (!isPasswordMatched) {
     throw new AppError(httpStatus.FORBIDDEN, 'Password not match!');
   }
-  console.log(isUserExists);
-  return [];
+
+  //create token and sent to the  client
+
+  const jwtPayload = {
+    userId: user.id,
+    role: user.role,
+  };
+
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+
+  return { accessToken };
 };
 
 export const authServices = {
